b5b158414eb1b4d56b7f03051832ebbb6787843a
masud <masud.ahmed92@gmail.com>
2015-04-22 15:19:17 +0600

    functionality updated and code refactor

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function($) {
+
+
+  $(document).ready(function(){
+
+    $(document).on("click",".fa-icons-list li",function(){
+      $(".fa-icons-list li").removeClass("selected");
+      $(this).addClass("selected");
+    });
+
+    $(document).on("click",".icon-insert-button", function(){
+      var $selectedIcon = $(".fa-icons-list li.selected span");
+      var icon_class = $selectedIcon.attr('class');
+      var icon_size = $selectedIcon.attr('data-size');
+
+      if(icon_class) $("#icon-input").val(icon_class+" "+icon_size);
+
+      // console.log($("#icon-input"))
+      $("#myModal").modal('hide');
+    });
+
+
+  });
+
+
+
+})(jQuery);
