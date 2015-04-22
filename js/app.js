ecf21fbd563a4ec9a4c1dfd11eb70ffa64fdb9b6
Parvez Akther <parvez@themexpert.com>
2015-04-22 19:26:48 +0600

    markup update

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@
   $(document).ready(function(){
 
     $(document).on("click",".fa-icons-list li",function(){
-      $(".fa-icons-list li").removeClass("selected");
-      $(this).addClass("selected");
+      $(".fa-icons-list li").removeClass("active");
+      $(this).addClass("active");
     });
 
     $(document).on("click",".icon-insert-button", function(){
-      var $selectedIcon = $(".fa-icons-list li.selected span");
-      var icon_class = $selectedIcon.attr('class');
-      var icon_size = $selectedIcon.attr('data-size');
+      var $activeIcon = $(".fa-icons-list li.active span");
+      var icon_class = $activeIcon.attr('class');
+      var icon_size = $activeIcon.attr('data-size');
 
       if(icon_class) $("#icon-input").val(icon_class+" "+icon_size);
       $("#iconModal").modal('hide');
